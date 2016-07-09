9f22461ada276e2da8d7da9893b59938717b1af1
Alex Slynko <oleksandr.o.slynko@gmail.com>
2016-07-09 14:21:11 +0300

    in process

---
+++ src/main/java/com/slynko/web/json/ChatMessageDecoder.java
@@ -0,0 +1,37 @@
+package com.slynko.web.json;
+
+import com.slynko.db.model.ChatMessage;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.websocket.DecodeException;
+import javax.websocket.Decoder;
+import javax.websocket.EndpointConfig;
+import java.io.StringReader;
+import java.util.Date;
+
+public class ChatMessageDecoder implements Decoder.Text<ChatMessage> {
+
+    @Override
+    public void init(final EndpointConfig config) {
+    }
+
+    @Override
+    public void destroy(){
+    }
+
+    public ChatMessage decode(final String textMessage) throws DecodeException {
+        ChatMessage chatMessage = new ChatMessage();
+        JsonObject jsonObject = Json.createReader(new StringReader(textMessage))
+                .readObject();
+        chatMessage.setMessage(jsonObject.getString("message"));
+        chatMessage.setSender(jsonObject.getString("sender"));
+        chatMessage.setReceived(new Date());
+        return chatMessage;
+    }
+
+    @Override
+    public boolean willDecode(final String s) {
+        return true;
+    }
+}
