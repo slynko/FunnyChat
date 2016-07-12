5cfc01171075dc554f86e01375dfefedd0b29523
Aleksandr Slynko <aslynko@connecture.com>
2016-07-12 20:13:43 +0300

    date convert

--- src/main/java/com/slynko/web/json/ChatMessageEncoder.java
+++ src/main/java/com/slynko/web/json/ChatMessageEncoder.java
@@ -6,6 +6,8 @@ import javax.json.Json;
 import javax.websocket.EncodeException;
 import javax.websocket.Encoder;
 import javax.websocket.EndpointConfig;
+import java.util.Calendar;
+import java.util.Date;
 
 public class ChatMessageEncoder implements Encoder.Text<ChatMessage> {
     @Override
@@ -21,7 +23,19 @@ public class ChatMessageEncoder implements Encoder.Text<ChatMessage> {
         return Json.createObjectBuilder()
                 .add("message", chatMessage.getMessage())
                 .add("sender", chatMessage.getSender())
-                .add("received", chatMessage.getReceived().toString()).build()
+                .add("received", buildDateReceived(chatMessage.getReceived())).build()
                 .toString();
+    }
+
+    private String buildDateReceived(Date dateReceived) {
+        Calendar calendarReceived = Calendar.getInstance();
+        calendarReceived.setTime(dateReceived);
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(calendarReceived.get(Calendar.HOUR_OF_DAY))
+                        .append(":").append(calendarReceived.get(Calendar.MINUTE))
+                        .append(" ").append(calendarReceived.get(Calendar.DAY_OF_MONTH))
+                        .append("/").append(calendarReceived.get(Calendar.MONTH))
+                        .append("/").append(calendarReceived.get(Calendar.YEAR));
+        return stringBuilder.toString();
     }
 }
